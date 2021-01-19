4f84c9dd2c1666b9959ce333716277c34c3f5094
SnowmanFTW <SnowmanFTW>
2021-01-19 23:54:53 +0200

    Cleaned up main menu, added setItems methods for Menu.java

---
+++ src/main/java/me/snowman/permgui2/commands/Perms.java
@@ -0,0 +1,22 @@
+package me.snowman.permgui2.commands;
+
+import me.snowman.permgui2.PermGUI;
+import me.snowman.permgui2.managers.MenuManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Perms implements CommandExecutor {
+    private final MenuManager menuManager;
+
+    public Perms(MenuManager menuManager){
+        this.menuManager = menuManager;
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = (Player) sender;
+        menuManager.getMainMenu().open(player);
+        return true;
+    }
+}
---
+++ src/main/java/me/snowman/permgui2/managers/MenuManager.java
@@ -0,0 +1,61 @@
+package me.snowman.permgui2.managers;
+
+import me.snowman.permgui2.PermGUI;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class MenuManager {
+    private final PermGUI permGUI;
+    private final MessageManager messageManager;
+
+    private Map<UUID, Menu> menus = new HashMap<>();
+
+    public MenuManager(PermGUI permGUI, MessageManager messageManager){
+        this.permGUI = permGUI;
+        this.messageManager = messageManager;
+    }
+
+    public Map<UUID, Menu> getMenus() {
+        return menus;
+    }
+
+    public void setMenu(Player player, Menu menu){
+        menus.put(player.getUniqueId(), menu);
+    }
+
+    public Menu getMenu(Player player){
+        return getMenus().get(player.getUniqueId());
+    }
+
+    public Menu getMenu(String fileName){
+        File menuFile = new File(permGUI.getDataFolder(), "menus" + File.separator + fileName + ".yml");
+        if(!menuFile.exists()){
+            getServer().getConsoleSender().sendMessage(fileName + ".yml doesn't exist!");
+            return null;
+        }
+        FileConfiguration menu = YamlConfiguration.loadConfiguration(menuFile);
+        return new Menu()
+                .set
+                .setTitle(messageManager.color(menu.getString("title")))
+                .setSize(menu.getInt("size"))
+                .setCommands(menu.getStringList("open-commands"))
+                .setInventoryType(InventoryType.valueOf(menu.getString("inventory-type")))
+                .setListType(menu.getString("list"))
+                .build();
+    }
+
+    public Menu getMainMenu(){
+        String menu = permGUI.getConfig().getString("MainMenu").replace(".yml", "");
+        return getMenu(menu);
+    }
+}
