413526499d7d81e62aba111b581ddb08c4ea48b4
SnowmanFTW <SnowmanFTW>
2021-01-20 21:38:44 +0200

    Done [OPEN] action and setting items in menus

--- src/main/java/me/snowman/permgui2/events/GUIListeners.java
+++ src/main/java/me/snowman/permgui2/events/GUIListeners.java
@@ -1,5 +1,7 @@
 package me.snowman.permgui2.events;
 
+import me.snowman.permgui2.managers.Menu;
+import me.snowman.permgui2.managers.MenuItem;
 import me.snowman.permgui2.managers.MenuManager;
 import me.snowman.permgui2.managers.PermsManager;
 import org.bukkit.entity.Player;
@@ -20,6 +22,18 @@ public class GUIListeners implements Listener {
     public void onClick(InventoryClickEvent event) {
         Player player = (Player) event.getWhoClicked();
         if (!menuManager.hasMenu(player)) return;
+        MenuItem item = new MenuItem(event.getCurrentItem());
+
+        for (String actions : item.getActions()) {
+            String action = actions.substring(0, actions.indexOf(" "));
+            String menuName = actions.substring(action.indexOf(" "));
+            Menu menu = menuManager.getMenu(menuName);
+            switch (action) {
+                case "[OPEN]":
+                    menuManager.open(player, menu);
+                    break;
+            }
+        }
         event.setCancelled(true);
     }
 }
