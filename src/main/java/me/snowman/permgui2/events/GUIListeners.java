9c323b6e262d02ebf1da4e288f43d7c28d0d84dd
SnowmanFTW <SnowmanFTW>
2021-01-20 22:08:17 +0200

    Fixed [OPEN] action, changed handling of menu files

--- src/main/java/me/snowman/permgui2/events/GUIListeners.java
+++ src/main/java/me/snowman/permgui2/events/GUIListeners.java
@@ -1,9 +1,6 @@
 package me.snowman.permgui2.events;
 
-import me.snowman.permgui2.managers.Menu;
-import me.snowman.permgui2.managers.MenuItem;
-import me.snowman.permgui2.managers.MenuManager;
-import me.snowman.permgui2.managers.PermsManager;
+import me.snowman.permgui2.managers.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,26 +8,29 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class GUIListeners implements Listener {
     private final MenuManager menuManager;
+    private final ItemManager itemManager;
     private final PermsManager permsManager;
 
-    public GUIListeners(MenuManager menuManager, PermsManager permsManager) {
+    public GUIListeners(MenuManager menuManager, ItemManager itemManager, PermsManager permsManager) {
         this.menuManager = menuManager;
         this.permsManager = permsManager;
+        this.itemManager = itemManager;
     }
 
     @EventHandler
     public void onClick(InventoryClickEvent event) {
         Player player = (Player) event.getWhoClicked();
         if (!menuManager.hasMenu(player)) return;
-        MenuItem item = new MenuItem(event.getCurrentItem());
+        Menu menu = menuManager.getMenu(player);
+        MenuItem item = itemManager.getItem(menu, event.getCurrentItem());
 
         for (String actions : item.getActions()) {
             String action = actions.substring(0, actions.indexOf(" "));
-            String menuName = actions.substring(action.indexOf(" "));
-            Menu menu = menuManager.getMenu(menuName);
+            String arguments = actions.substring(actions.indexOf(" ") + 1);
             switch (action) {
                 case "[OPEN]":
-                    menuManager.open(player, menu);
+                    Menu openedMenu = menuManager.getMenu(arguments);
+                    menuManager.open(player, openedMenu);
                     break;
             }
         }
---
+++ src/main/java/me/snowman/permgui2/managers/ItemManager.java
@@ -0,0 +1,50 @@
+package me.snowman.permgui2.managers;
+
+import me.snowman.permgui2.PermGUI;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class ItemManager {
+    private final PermGUI permGUI;
+    private final MessageManager messageManager;
+
+    public ItemManager(PermGUI permGUI, MessageManager messageManager) {
+        this.permGUI = permGUI;
+        this.messageManager = messageManager;
+    }
+
+    public List<MenuItem> getItems(String fileName) {
+        File menuFile = new File(permGUI.getDataFolder(), "menus" + File.separator + fileName);
+        if (!menuFile.exists()) {
+            getServer().getConsoleSender().sendMessage(fileName + " doesn't exist!");
+            return null;
+        }
+        FileConfiguration menu = YamlConfiguration.loadConfiguration(menuFile);
+        ConfigurationSection itemsSection = menu.getConfigurationSection("items");
+        List<MenuItem> items = new ArrayList<>();
+        itemsSection.getKeys(false)
+                .forEach(item -> items.add(new MenuItem(Material.matchMaterial(itemsSection.getString(item + ".material")), itemsSection.getInt(item + ".amount"))
+                        .setName(messageManager.color(itemsSection.getString(item + ".name")))
+                        .setLore(messageManager.color(itemsSection.getStringList(item + ".lore")))
+                        .setActions(itemsSection.getStringList(item + ".actions"))
+                        .setSlot(itemsSection.getInt(item + ".slot"))
+                        .build()));
+        return items;
+    }
+
+    public MenuItem getItem(Menu menu, ItemStack itemStack) {
+        for (MenuItem item : menu.getItems()) {
+            if (item.getItem().isSimilar(itemStack)) return item;
+        }
+        return null;
+    }
+}
