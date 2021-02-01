66b0b06ddf3b2409230671524a6b121559e9e6b0
Snowman <snowmanftwyt@gmail.com>
2021-02-01 21:53:03 +0200

    Whole plugin cleanup code

--- src/main/java/me/snowman/permgui2/PermGUI.java
+++ src/main/java/me/snowman/permgui2/PermGUI.java
@@ -4,7 +4,6 @@ import me.snowman.permgui2.commands.Perms;
 import me.snowman.permgui2.events.ChatListeners;
 import me.snowman.permgui2.events.GUIListeners;
 import me.snowman.permgui2.managers.*;
-import me.snowman.permgui2.permissions.PermissionModify;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PermGUI extends JavaPlugin {
@@ -17,7 +16,6 @@ public class PermGUI extends JavaPlugin {
         final UserManager userManager = new UserManager();
         final ItemManager itemManager = new ItemManager(this, messageManager);
         final MenuManager menuManager = new MenuManager(this, itemManager, messageManager, permsManager, userManager);
-        final PermissionModify permissionModify = new PermissionModify(permsManager);
         getCommand("perms2").setExecutor(new Perms(menuManager, userManager));
         getServer().getPluginManager().registerEvents(new GUIListeners(menuManager, itemManager, permsManager, userManager, messageManager), this);
         getServer().getPluginManager().registerEvents(new ChatListeners(messageManager, permsManager, userManager), this);
--- src/main/java/me/snowman/permgui2/permissions/PermissionAction.java
+++
@@ -1,9 +0,0 @@
-package me.snowman.permgui2.permissions;
-
-public enum PermissionAction {
-    CHANGEGROUP,
-    ADDGROUP,
-    PREFIX,
-    SUFFIX,
-    CREATEGROUP
-}
