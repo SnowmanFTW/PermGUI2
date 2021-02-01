8038bc8776afe356e09362d8ab2a005d4684f58e
Snowman <snowmanftwyt@gmail.com>
2021-02-01 22:01:33 +0200

    More cleaning up

--- src/main/java/me/snowman/permgui2/managers/PermsManager.java
+++ src/main/java/me/snowman/permgui2/managers/PermsManager.java
@@ -1,26 +1,15 @@
 package me.snowman.permgui2.managers;
 
 import me.snowman.permgui2.PermGUI;
-import me.snowman.permgui2.permissions.PermissionAction;
-import me.snowman.permgui2.permissions.PermissionModify;
-import net.luckperms.api.LuckPerms;
-import net.luckperms.api.LuckPermsProvider;
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.permission.Permission;
 import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class PermsManager {
     private final PermGUI permGUI;
     private static Permission perms = null;
     private static Chat chat = null;
-    private final List<PermissionModify> permissionModifyList = new ArrayList<>();
     public PermsManager(PermGUI permGUI){
         this.permGUI = permGUI;
     }
@@ -64,80 +53,4 @@ public class PermsManager {
     public Chat getChat(){
         return chat;
     }
-
-    public void createPermissionModify(Player player, Player target, PermissionAction action, String string, boolean waiting){
-        PermissionModify permissionModify = new PermissionModify(player, target, action, string, waiting);
-        if(getPermissionModify(player) != null) permissionModifyList.remove(getPermissionModify(player));
-        permissionModifyList.add(permissionModify);
-    }
-
-    public void createPermissionModify(Player player, Player target, PermissionAction action, String string){
-        PermissionModify permissionModify = new PermissionModify(player, target, action, string);
-        if(getPermissionModify(player) != null) permissionModifyList.remove(getPermissionModify(player));
-        permissionModifyList.add(permissionModify);
-    }
-
-    public void createPermissionModify(Player player, Player target){
-        PermissionModify permissionModify = new PermissionModify(player, target);
-        if(getPermissionModify(player) != null) permissionModifyList.remove(getPermissionModify(player));
-        permissionModifyList.add(permissionModify);
-    }
-
-    public void createPermissionModify(Player player, String group, PermissionAction action, String string, boolean waiting){
-        PermissionModify permissionModify = new PermissionModify(player, group, action, string, waiting);
-        if(getPermissionModify(player) != null) permissionModifyList.remove(getPermissionModify(player));
-        permissionModifyList.add(permissionModify);
-    }
-
-    public void createPermissionModify(Player player, String group, PermissionAction action, String string){
-        PermissionModify permissionModify = new PermissionModify(player, group, action, string);
-        if(getPermissionModify(player) != null) permissionModifyList.remove(getPermissionModify(player));
-        permissionModifyList.add(permissionModify);
-    }
-
-    public void createPermissionModify(Player player, String group){
-        PermissionModify permissionModify = new PermissionModify(player, group);
-        if(getPermissionModify(player) != null) permissionModifyList.remove(getPermissionModify(player));
-        permissionModifyList.add(permissionModify);
-    }
-
-    public PermissionModify getPermissionModify(Player player){
-        for(PermissionModify permissionModify: permissionModifyList){
-            if(permissionModify.getPlayer().getName().equals(player.getName())){
-                return permissionModify;
-            }
-        }
-        return null;
-    }
-
-    public void action(PermissionModify permissionModify){
-        PermissionAction action = permissionModify.getAction();
-        Player target = permissionModify.getTarget();
-        String group = permissionModify.getGroup();
-        String string = permissionModify.getString();
-        switch(action){
-            case ADDGROUP:
-                getPerms().playerAddGroup(null, target, string);
-                break;
-            case CHANGEGROUP:
-                Arrays.stream(getPerms().getPlayerGroups(null, target)).forEach(groups -> getPerms().playerRemoveGroup(null, target, groups));
-                getPerms().playerAddGroup(null, target, string);
-                break;
-            case PREFIX:
-                if(target != null) getChat().setPlayerPrefix(null, target, string);
-                else if(group != null) getChat().setGroupPrefix((World) null, group, string);
-                break;
-            case SUFFIX:
-                if(target != null) getChat().setPlayerSuffix(null, target, string);
-                else if(group != null) getChat().setGroupSuffix((World) null, group, string);
-                break;
-            case CREATEGROUP:
-                if(Bukkit.getServer().getPluginManager().isPluginEnabled("LuckPerms")){
-                    LuckPerms api = LuckPermsProvider.get();
-                    api.getGroupManager().createAndLoadGroup(string);
-                }
-                break;
-        }
-    }
-            
 }
