bcecbee71c0f46f2c26392eb182f3953224110f6
Snowman <snowmanftwyt@gmail.com>
2021-02-07 22:57:45 +0200

    added more messages, added update checker

--- src/main/java/me/snowman/permgui2/PermGUI.java
+++ src/main/java/me/snowman/permgui2/PermGUI.java
@@ -6,8 +6,15 @@ import me.snowman.permgui2.commands.Perms;
 import me.snowman.permgui2.events.ChatListeners;
 import me.snowman.permgui2.events.GUIListeners;
 import me.snowman.permgui2.managers.*;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.concurrent.Callable;
 
 public class PermGUI extends JavaPlugin {
@@ -29,11 +36,16 @@ public class PermGUI extends JavaPlugin {
 
         permsManager.setupChat();
         permsManager.setupPermissions();
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&bPermGUI2 &f- &bLoaded Vault."));
         fileManager.setupConfig();
         fileManager.setupMessages();
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&bPermGUI2 &f- &bLoaded all configs."));
         fileManager.setupGUIs();
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&bPermGUI2 &f- &bLoaded all menus."));
         Metrics metrics = new Metrics(this, id);
         addCharts(metrics, permsManager);
+        updatePlugin();
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&bPermGUI2 &f- &bPlugin loaded. v&f" + getDescription().getVersion()));
     }
 
     @Override
@@ -47,6 +59,32 @@ public class PermGUI extends JavaPlugin {
 
     public static PermGUIAPI getAPI(){
         return new PermGUIAPI();
+    }
+
+    public void updatePlugin() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                try {
+                    String spigotId = "70350";
+                    // Open connection with spigot's web API
+                    HttpsURLConnection connection = (HttpsURLConnection) new URL("https://api.spigotmc.org/legacy/update.php?resource=" + spigotId).openConnection();
+
+                    // Versions
+                    String latest = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine();
+                    String installed = getDescription().getVersion();
+
+                    // Check if it's outdated or not
+                    if (!installed.equals(latest)) {
+                        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&bPermGUI2 &f- &bYour plugin version (&f" + installed + "&b) is not the latest one (&f" + latest + "&b)\n&bYou can download it here: &fhttps://www.spigotmc.org/resources/70350/"));
+                    } else {
+                        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&bPermGUI2 &f- &bPlugin version is up-to-date (&f" + installed + "&b)."));
+                    }
+                } catch (Exception e) {
+                    Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&bPermGUI2 &f- &fThere was an error connecting to SpigotMC API."));
+                }
+            }
+        }.runTaskLaterAsynchronously(this, 40);
     }
 
 
